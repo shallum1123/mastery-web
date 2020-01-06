7381f8ecaf69da10237d00db2758ac18f0dd685a
baaps1993 <1483503519@qq.com>
2020-01-06 14:47:15 +0800

    Modify the article to create  time ordered reverse query

--- src/main/java/com/ytxd/contorller/UEditorController.java
+++ src/main/java/com/ytxd/contorller/UEditorController.java
@@ -1,7 +1,6 @@
 package com.ytxd.contorller;
 
 import com.baidu.ueditor.ActionEnter;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
